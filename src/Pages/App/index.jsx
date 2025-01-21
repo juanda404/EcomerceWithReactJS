bf0dae8ab9710227d1bf03d0d83e83674761dd11
Juan David S <juanda404@gmail.com>
2025-01-21 11:35:34 -0500

    Enrutamiento con React Router DOM

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,24 +1,29 @@
-import Home from '../Home';
-import MyAccount   from '../MyAccount';
-import MyOrder from '../MyOrder';
-import MyOrders from '../MyOrders';
-import NotFound from '../NotFound';
-import SigIn from '../SigIn';
+import {useRoutes, BrowserRouter} from 'react-router-dom'
+import Home from '../Home'
+import MyAccount from '../MyAccount'
+import MyOrder from '../MyOrder'
+import MyOrders from '../MyOrders'
+import NotFound from '../NotFound'
+import SignIn from '../SigIn'
+import './App.css'
 
-import './App.css';
-
-function App() {
-
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: '/',  element: <Home />},
+    { path: '/my-account',  element: <MyAccount />},
+    { path: '/my-order',  element: <MyOrder />},
+    { path: '/my-orders',  element: <MyOrders />},
+    { path: '/signIn',  element: <SignIn />},
+    { path: '/*',  element: <NotFound />},
+ 
+  ])
+  return routes
+}
+const  App = () =>{
   return (
-      <div className="bg-lime-400	">
-            <Home />
-            <MyAccount />
-            <MyOrder />
-            <MyOrders />
-            <NotFound />
-            <SigIn />
-      </div>
+    <BrowserRouter>
+        <AppRoutes />
+    </BrowserRouter>
   )
 }
-
 export default App
