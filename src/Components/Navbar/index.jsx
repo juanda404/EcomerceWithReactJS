a7293b0815de95a069ea871a7a028e71b3d2bcf8
Juan David S <juanda404@gmail.com>
2025-01-21 21:04:15 -0500

    contador de productos en el carrito

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,6 +1,9 @@
-import { NavLink } from "react-router-dom"
+import { useContext } from 'react'
+import { NavLink } from  'react-router-dom'
+import{ ShoppingCartContext} from '../../Context'
 
 const Navbar = () =>{
+    const context = useContext(ShoppingCartContext)
     const  activeStyle ='underline underline-offset-4'
     return (
         <nav className="flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light">
@@ -94,7 +97,7 @@ const Navbar = () =>{
                   </NavLink>
                 </li>
                 <li>
-                 🛍 0
+                 🛍 {context.count } 
                 </li>
             </ul>
         </nav>
