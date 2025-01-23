3d1b1ebe628add8bc74fcdd667b16d339b680c35
Juan David S <juanda404@gmail.com>
2025-01-23 17:57:09 -0500

    formulario para crear usuario

--- src/Pages/SigIn/index.jsx
+++ src/Pages/SigIn/index.jsx
@@ -1,10 +1,12 @@
-import { useContext } from 'react'
+import { useContext, useState, useRef} from 'react'
 import { Link} from 'react-router-dom'
 import { ShoppingCartContext } from '../../Context'
 import Layout from '../../Components/Layout'
 
 function SigIn() {
       const context = useContext(ShoppingCartContext)
+      const [view, setView] = useState('user-info')
+      const form = useRef(null)
 
       //Account 
       const account = localStorage.getItem('account')
@@ -13,10 +15,20 @@ function SigIn() {
       const noAccountInLocalStorage = parsedAccount ? Object.keys(parsedAccount).length === 0 : true
       const noAccountInLocalState = context.account ? Object.keys(context.account).length === 0 : true
       const hasUserAnAccount = !noAccountInLocalStorage || !noAccountInLocalState
+ 
+      const createAnAccount = () => {
+		const formData = new FormData(form.current)
+		const data = {
+			name: formData.get('name'),
+			email: formData.get('email'),
+			password: formData.get('password')
+		}
+                   // TODO: Remove this console.log 🥲
+		console.log(data)
+	}
 
-  return (
-      <Layout>
-            <h1 className='font-medium text-xl text-center mb-6 w-80'>Welcome</h1>
+      const renderLogIn = () =>{
+      return (
             <div className='flex flex-col w-80'>
                  <p>
                       <span className='font-light text-sm'>Email: </span>
@@ -39,10 +51,70 @@ function SigIn() {
                   </div>
                   <button
                   className='border border-black disabled:text-black/40 disabled:border-black/40 rounded-lg mt-6 py-3'
+                  onClick={() => setView('create-user-info')}
                   disabled={hasUserAnAccount}>
                   Sign up
                   </button>
             </div>
+      )
+}
+
+const renderCreateUserInfo =() =>{
+      return (
+            <form ref={form} className='flex flex-col gap-4 w-80'>
+              <div className='flex flex-col gap-1'>
+                <label htmlFor="name" className='font-light text-sm'>Your name:</label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  defaultValue={parsedAccount?.name}
+                  placeholder="Peter"
+                  className='rounded-lg border border-black placeholder:font-light
+                  placeholder:text-sm placeholder:text-black/60 focus:outline-none py-2 px-4'
+                />
+              </div>
+              <div className='flex flex-col gap-1'>
+                <label htmlFor="email" className='font-light text-sm'>Your email:</label>
+                <input
+                  type="text"
+                  id="email"
+                  name="email"
+                  defaultValue={parsedAccount?.email}
+                  placeholder="hi@helloworld.com"
+                  className='rounded-lg border border-black
+                  placeholder:font-light placeholder:text-sm placeholder:text-black/60 focus:outline-none py-2 px-4'
+                />
+              </div>
+              <div className='flex flex-col gap-1'>
+                <label htmlFor="password" className='font-light text-sm'>Your password:</label>
+                <input
+                  type="text"
+                  id="password"
+                  name="password"
+                  defaultValue={parsedAccount?.password}
+                  placeholder="******"
+                  className='rounded-lg border border-black
+                  placeholder:font-light placeholder:text-sm placeholder:text-black/60 focus:outline-none py-2 px-4'
+                />
+              </div>
+              <Link to="/">
+                <button
+                  className='bg-black text-white w-full rounded-lg py-3'
+                  onClick={() => createAnAccount()}>
+                  Create
+                </button>
+              </Link>
+            </form>
+          )
+}
+
+const renderView = () => view === 'create-user-info' ? renderCreateUserInfo() : renderLogIn()
+  
+return(
+      <Layout>
+          <h1 className="font-medium text-xl text-center mb-6 w-80">Welcome</h1>
+          {renderView()}
       </Layout>
   )
 }
